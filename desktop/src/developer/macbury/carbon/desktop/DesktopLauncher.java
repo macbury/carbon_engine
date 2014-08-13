2f88cdd56011f769d3cf75812a83e4cb315d2a5c
Arkadiusz Buras <macbury@gmail.com>
2014-08-13 12:42:50 +0200

    Golden commit

---
+++ desktop/src/developer/macbury/carbon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package developer.macbury.carbon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import developer.macbury.carbon.CarbonEngine;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CarbonEngine(), config);
+	}
+}
